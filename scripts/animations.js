59d028cf83cf3f887fa1188f5dfc435673318702
Literall <pnguyen@icgconsulting.com>
2017-11-11 00:11:05 -0700

    changed spa

--- scripts/animations.js
+++
@@ -1,9 +0,0 @@
-$(function (){
-    setTimeout(function(){
-      $('.fly-in-text').removeClass('hidden');
-    }, 500);
-  });
-
-  function toggleSidebar() {
-      document.getElementById('side-bar').classList.toggle('active');
-  }
